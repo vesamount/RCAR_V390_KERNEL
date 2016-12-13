d229393edbe5ebd533076dfcb9ae4eaeb236ee22
Andrey Gusakov <andrey.gusakov@cogentembedded.com>
2016-12-13 18:07:13 +0300

    ASoC: add dummy Si468x driver
    Signed-off-by: Andrey Gusakov <andrey.gusakov@cogentembedded.com>

---
+++ sound/soc/codecs/si468x.c
@@ -0,0 +1,66 @@
+/*
+ * Dummy sound driver for Si468x DAB/FM/AM chips
+ * Copyright 2016 Andrey Gusakov <andrey.gusakov@cogentembedded.com>
+ *
+ * Based on: Driver for the DFBM-CS320 bluetooth module
+ * Copyright 2011 Lars-Peter Clausen <lars@metafoo.de>
+ *
+ *  This program is free software; you can redistribute  it and/or modify it
+ *  under  the terms of  the GNU General  Public License as published by the
+ *  Free Software Foundation;  either version 2 of the  License, or (at your
+ *  option) any later version.
+ *
+ */
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/platform_device.h>
+
+#include <sound/soc.h>
+
+static struct snd_soc_dai_driver si468x_dai = {
+	.name = "si468x-pcm",
+	.capture = {
+		.channels_min = 2,
+		.channels_max = 2,
+		.rates = SNDRV_PCM_RATE_48000,
+		.formats = SNDRV_PCM_FMTBIT_S16_LE,
+	},
+};
+
+static struct snd_soc_codec_driver soc_codec_dev_si468x;
+
+static int si468x_probe(struct platform_device *pdev)
+{
+	return snd_soc_register_codec(&pdev->dev, &soc_codec_dev_si468x,
+			&si468x_dai, 1);
+}
+
+static int si468x_remove(struct platform_device *pdev)
+{
+	snd_soc_unregister_codec(&pdev->dev);
+
+	return 0;
+}
+
+static const struct of_device_id si468x_of_match[] = {
+	{ .compatible = "si,si468x-pcm", },
+	{ }
+};
+MODULE_DEVICE_TABLE(of, si468x_of_match);
+
+static struct platform_driver si468x_driver = {
+	.driver = {
+		.name = "si468x",
+		.of_match_table = si468x_of_match,
+		.owner = THIS_MODULE,
+	},
+	.probe = si468x_probe,
+	.remove = si468x_remove,
+};
+
+module_platform_driver(si468x_driver);
+
+MODULE_AUTHOR("Andrey Gusakov <andrey.gusakov@cogentembedded.com>");
+MODULE_DESCRIPTION("ASoC Si468x radio chip driver");
+MODULE_LICENSE("GPL");
