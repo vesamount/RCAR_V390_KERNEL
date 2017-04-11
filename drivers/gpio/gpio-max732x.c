54c08b7ee4474172ca2b99927fb2429f204122a3
Vladimir Barinov <vladimir.barinov@cogentembedded.com>
2017-04-11 20:12:56 +0300

    gpio: max732x: fix gpio set
    gpio set value/direction must 0 or 1, but
    gpiolib sets it to not binary values
    
    Signed-off-by: Vladimir Barinov <vladimir.barinov@cogentembedded.com>

--- drivers/gpio/gpio-max732x.c
+++ drivers/gpio/gpio-max732x.c
@@ -237,7 +237,7 @@ static void max732x_gpio_set_value(struct gpio_chip *gc, unsigned off, int val)
 	unsigned base = off & ~0x7;
 	uint8_t mask = 1u << (off & 0x7);
 
-	max732x_gpio_set_mask(gc, base, mask, val << (off & 0x7));
+	max732x_gpio_set_mask(gc, base, mask, (!!val) << (off & 0x7));
 }
 
 static void max732x_gpio_set_multiple(struct gpio_chip *gc,
